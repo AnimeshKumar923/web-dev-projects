c83a651caa7a6cfeb053c5ae173a33c0d7590fcb
Animesh Kumar <kumaranimesh923@gmail.com>
2024-01-26 21:42:53 +0530

    chore: improve game logic

--- 03-rock-paper-scissors/script.js
+++ 03-rock-paper-scissors/script.js
@@ -1,68 +1,85 @@
 // assigning rock = 1, paper = 2, scissors = 3
 
-let computerSelection = getComputerChoice();
-let playerSelection = prompt("Your turn");
+// let playerChoiceNum = playerChoice();
+// var computerChoiceNum = getComputerChoice();
 
-let lowercasePlayer = playerSelection.toLowerCase();
-let playerChoiceNum;
-if(lowercasePlayer == "rock"){
-    playerChoiceNum = 1;
-} else if(lowercasePlayer == "paper"){
-    playerChoiceNum = 2;
-} else if(lowercasePlayer == "scissors"){
-    playerChoiceNum = 3;
-} else{
-    console.log(`Please enter correct choice!`);
-}
-
-gameRound(playerChoiceNum, computerChoiceNum);
-
-var computerChoiceNum;
-// console.log(computerChoiceNum);
+// let result = playRound(playerChoiceNum, computerChoiceNum);
 
 function getComputerChoice(){
     let number = getRandom(1,3);
     switch(number){
         case 1:
             // obj = "Rock";
-            computerChoiceNum = 1;
-            return;
+            return 1;
         case 2:
             // obj = "Paper";
-            computerChoiceNum = 2;
-            return;
+            return 2;
         case 3:
             // obj = "Scissors";
-            computerChoiceNum = 3;
-            return;
+            return 3;
     }
 }
             
-            
-var isTie;
-// assigning rock = 1, paper = 2, scissors = 3
-function gameRound(playerChoiceNum, computerChoiceNum){
-    if((playerChoiceNum === 1 && computerChoiceNum === 1) || (playerChoiceNum === 2 && computerChoiceNum === 2) || (playerChoiceNum === 3 && computerChoiceNum === 3)){
-        console.log(`It's a tie!`);
-        isTie = true;
-    } else if((playerChoiceNum === 1 && computerChoiceNum === 2)){
-        console.log(`Computer wins!`);
-    } else if((playerChoiceNum === 1 && computerChoiceNum === 3)){
-        console.log(`Player wins!`);
-    } else if((playerChoiceNum === 2 && computerChoiceNum === 1)){
-        console.log(`Player wins!`);
-    } else if((playerChoiceNum === 2 && computerChoiceNum === 3)){
-        console.log(`Computer wins!`);
-    } else if((playerChoiceNum === 3 && computerChoiceNum === 1)){
-        console.log(`Computer wins!`);
-    } else if((playerChoiceNum === 3 && computerChoiceNum === 2)){
-        console.log(`Player wins!`);
+function playerChoice(){
+    let playerSelection = prompt("Your turn");
+    let lowercasePlayer = playerSelection.toLowerCase();
+    let playerChoiceNum;
+    if(lowercasePlayer == "rock"){
+        return playerChoiceNum = 1;
+    } else if(lowercasePlayer == "paper"){
+        return playerChoiceNum = 2;
+    } else if(lowercasePlayer == "scissors"){
+        return playerChoiceNum = 3;
+    } else{
+        console.log(`Please enter correct choice!`);
     }
 }
 
-// if(isTie){
-//     gameRound(playerChoiceNum, computerChoiceNum);
-// }
+// assigning rock = 1, paper = 2, scissors = 3
+var isTie;
+var computerWinCount = 0;
+var playerWinCount = 0;
+
+game(playerWinCount, computerWinCount);
+
+function playRound(playerChoiceNum, computerChoiceNum, computerWinCount, playerWinCount){
+    if((playerChoiceNum === computerChoiceNum)){
+        isTie = true;
+        playerWinCount++;
+        computerWinCount++;
+        return (`It's a tie!`);
+    } else if((playerChoiceNum === 1 && computerChoiceNum === 2)){
+        isTie = false;
+        return (`Computer wins!`);
+    } else if((playerChoiceNum === 1 && computerChoiceNum === 3)){
+        isTie = false;
+        playerWinCount++;
+        return (`You win!`);
+    } else if((playerChoiceNum === 2 && computerChoiceNum === 1)){
+        isTie = false;
+        playerWinCount++;
+        return (`You win!`);
+    } else if((playerChoiceNum === 2 && computerChoiceNum === 3)){
+        isTie = false;
+        return (`Computer wins!`);
+    } else if((playerChoiceNum === 3 && computerChoiceNum === 1)){
+        isTie = false;
+        return (`Computer wins!`);
+    } else if((playerChoiceNum === 3 && computerChoiceNum === 2)){
+        isTie = false;
+        playerWinCount++;
+        return (`You win!`);
+    }
+}
+
+
+while(isTie){
+    alert(`It's a tie! Try Again`);
+    playerChoiceNum = playerChoice();
+    computerChoiceNum = getComputerChoice();
+    result = playRound(playerChoiceNum, computerChoiceNum, computerWinCount, playerWinCount);
+}
+// console.log(result);
 
 function getRandom(min, max) {
     const floatRandom = Math.random();
@@ -75,4 +92,24 @@ function getRandom(min, max) {
     const randomWithinRange = random + min;
   
     return randomWithinRange;
+}
+
+function game(playerWinCount, computerWinCount){
+    let playerChoiceNum;
+    let computerChoiceNum;
+    for(i=0; i<5; i++){
+        playerChoiceNum = playerChoice();
+        computerChoiceNum = getComputerChoice();
+        let result = playRound(playerChoiceNum, computerChoiceNum, computerWinCount, playerWinCount);
+    }
+
+    if(playerWinCount>computerWinCount){
+        console.log(`Player: ${playerWinCount}
+        Computer: ${computerWinCount}`);
+        console.log(`Player wins the round!`);
+    } else{
+        console.log(`Player: ${playerWinCount}
+        Computer: ${computerWinCount}`);
+        console.log(`Computer wins the round!`);
+    }
 }
