06ab426872e584a79440e17b57b24f261aaa39c0
Animesh Kumar <kumaranimesh923@gmail.com>
2024-01-27 00:55:05 +0530

    chore: improve game logic

--- 03-rock-paper-scissors/script.js
+++ 03-rock-paper-scissors/script.js
@@ -1,10 +1,5 @@
 // assigning rock = 1, paper = 2, scissors = 3
 
-// let playerChoiceNum = playerChoice();
-// var computerChoiceNum = getComputerChoice();
-
-// let result = playRound(playerChoiceNum, computerChoiceNum);
-
 function getComputerChoice(){
     let number = getRandom(1,3);
     switch(number){
@@ -20,10 +15,12 @@ function getComputerChoice(){
     }
 }
             
+let playerChoiceNum;
+let computerChoiceNum;
+
 function playerChoice(){
     let playerSelection = prompt("Your turn");
     let lowercasePlayer = playerSelection.toLowerCase();
-    let playerChoiceNum;
     if(lowercasePlayer == "rock"){
         return playerChoiceNum = 1;
     } else if(lowercasePlayer == "paper"){
@@ -39,10 +36,9 @@ function playerChoice(){
 var isTie;
 var computerWinCount = 0;
 var playerWinCount = 0;
+let result;
 
-game(playerWinCount, computerWinCount);
-
-function playRound(playerChoiceNum, computerChoiceNum, computerWinCount, playerWinCount){
+function playRound(playerChoiceNum, computerChoiceNum){
     if((playerChoiceNum === computerChoiceNum)){
         isTie = true;
         playerWinCount++;
@@ -77,9 +73,8 @@ while(isTie){
     alert(`It's a tie! Try Again`);
     playerChoiceNum = playerChoice();
     computerChoiceNum = getComputerChoice();
-    result = playRound(playerChoiceNum, computerChoiceNum, computerWinCount, playerWinCount);
+    result = playRound(playerChoiceNum, computerChoiceNum);
 }
-// console.log(result);
 
 function getRandom(min, max) {
     const floatRandom = Math.random();
@@ -94,22 +89,24 @@ function getRandom(min, max) {
     return randomWithinRange;
 }
 
-function game(playerWinCount, computerWinCount){
-    let playerChoiceNum;
-    let computerChoiceNum;
+function game(computerWinCount, playerWinCount){
+
     for(i=0; i<5; i++){
         playerChoiceNum = playerChoice();
         computerChoiceNum = getComputerChoice();
-        let result = playRound(playerChoiceNum, computerChoiceNum, computerWinCount, playerWinCount);
+        result = playRound(playerChoiceNum, computerChoiceNum);
+        console.log(result);
     }
 
     if(playerWinCount>computerWinCount){
-        console.log(`Player: ${playerWinCount}
-        Computer: ${computerWinCount}`);
+        console.log(`Player: ${playerWinCount} Computer: ${computerWinCount}`);
         console.log(`Player wins the round!`);
-    } else{
-        console.log(`Player: ${playerWinCount}
-        Computer: ${computerWinCount}`);
+    } else if(playerWinCount<computerWinCount){
+        console.log(`Player: ${playerWinCount} Computer: ${computerWinCount}`);
         console.log(`Computer wins the round!`);
     }
 }
+
+game(computerWinCount, playerWinCount);
+
+console.log(`Player: ${playerWinCount} Computer: ${computerWinCount}`);
