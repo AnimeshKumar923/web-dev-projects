1bc2bd42f1fc53bb15e59245546256cd9c971b55
Animesh Kumar <kumaranimesh923@gmail.com>
2024-03-12 01:38:56 +0530

    fix: add prompt for asking size

--- 04-etch-a-sketch/script.js
+++ 04-etch-a-sketch/script.js
@@ -1,3 +1,8 @@
+// --------- whiteboard size ---------------
+let panelSize = prompt('Enter size', '16');
+
+// -----------------------------------------
+
 const body = document.querySelector('body');
 
 const parentContainer = document.createElement('div');
@@ -8,23 +13,7 @@ const rowDivCSSflexbox = `display: flex; flex: 1 1 0; flex-wrap: wrap;`;
 
 const rowDivCSStext = `border: 0px solid black; height: 150px; width: 135px; gap: 2px; ${rowDivCSSflexbox}`;
 
-for(let i=1; i<=16; i++){
-    const rowDiv = document.createElement('row-div');
-    rowDiv.classList.add(`row-div-${i}`)
-    rowDiv.style.cssText = rowDivCSStext;
-    parentContainer.appendChild(rowDiv);
-    for(let j=1; j<=16; j++){
-        const columnDiv = document.createElement('col-div');
-        columnDiv.classList.add(`col-div`)
-        columnDiv.style.cssText = 'border: 1px solid black; height: 30px; width: 30px';
-        rowDiv.appendChild(columnDiv);
-        
-        // window.addEventListener('DOMContentLoaded', function(){
-        //     // hoverForEach(j);
-        // });
-        
-    }
-}
+boxSetter();
 
 const hover = document.querySelectorAll(`.col-div`);
 hover.forEach(element => {
@@ -32,3 +21,105 @@ hover.forEach(element => {
         this.style.backgroundColor = `green`;
     });
 });
+
+
+// clear whiteboard button
+const clrBtn = document.createElement('button');
+clrBtn.classList.add('clear-btn');
+clrBtn.style.height = '50px'
+clrBtn.style.width = '50px'
+clrBtn.style.marginLeft = '50px'
+clrBtn.style.marginTop = '50px'
+clrBtn.textContent = 'Clear'
+body.appendChild(clrBtn)
+
+clrBtn.addEventListener('click', function(){
+    hover.forEach(element => {
+        element.style.backgroundColor = `white`;
+    }); 
+});
+
+
+// while(panelSize<=100){
+// }
+
+function boxSetter(){
+    for(let i=1; i<=panelSize; i++){
+        const rowDiv = document.createElement('row-div');
+        rowDiv.classList.add(`row-div`)
+        rowDiv.style.cssText = rowDivCSStext;
+        parentContainer.appendChild(rowDiv);
+        for(let j=1; j<=panelSize; j++){
+            const columnDiv = document.createElement('col-div');
+            columnDiv.classList.add(`col-div`)
+            columnDiv.style.cssText = 'border: 1px solid black; height: 30px; width: 30px';
+            rowDiv.appendChild(columnDiv);    
+        }
+    }
+}
+
+
+
+// for(let i=1; i<=16; i++){
+//     const rowDiv = document.createElement('row-div');
+//     rowDiv.classList.add(`row-div`)
+//     rowDiv.style.cssText = rowDivCSStext;
+//     parentContainer.appendChild(rowDiv);
+//     for(let j=1; j<=16; j++){
+//         const columnDiv = document.createElement('col-div');
+//         columnDiv.classList.add(`col-div`)
+//         columnDiv.style.cssText = 'border: 1px solid black; height: 30px; width: 30px';
+//         rowDiv.appendChild(columnDiv);    
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const panelBt = document.createElement('button');
+// panelBt.classList.add('clear-btn');
+// panelBt.style.height = '50px'
+// panelBt.style.width = '50px'
+// panelBt.style.marginLeft = '50px'
+// panelBt.style.marginTop = '50px'
+// panelBt.textContent = panelSize;
+// body.appendChild(panelBt)
