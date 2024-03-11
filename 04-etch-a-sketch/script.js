48e79192dd7befae4d60fc11feaf5d2f246ef1c4
Animesh Kumar <kumaranimesh923@gmail.com>
2024-03-12 00:22:18 +0530

    fix: add hover effect

--- 04-etch-a-sketch/script.js
+++ 04-etch-a-sketch/script.js
@@ -4,26 +4,31 @@ const parentContainer = document.createElement('div');
 parentContainer.classList.add('container')
 body.appendChild(parentContainer);
 
-// parentContainer.style.display = 'flex';
-// parentContainer.style.cssText = 'display: flex; flex: flex-wrap;'
+const rowDivCSSflexbox = `display: flex; flex: 1 1 0; flex-wrap: wrap;`;
 
-// for(let i=0; i<257; i++){
-//     const sqrDiv = document.createElement('div');
-//     sqrDiv.style.cssText = 'border: 1px solid black; height: 30px; width: 30px'
-//     parentContainer.appendChild(sqrDiv);
-// }
-
-const rowDivCSStext = `border: 1px solid black; height: 150px; width: 137px; gap: 2px; display: flex; flex-wrap: wrap`;
+const rowDivCSStext = `border: 0px solid black; height: 150px; width: 135px; gap: 2px; ${rowDivCSSflexbox}`;
 
 for(let i=1; i<=16; i++){
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add(`div-${i}`)
+    const rowDiv = document.createElement('row-div');
+    rowDiv.classList.add(`row-div-${i}`)
     rowDiv.style.cssText = rowDivCSStext;
     parentContainer.appendChild(rowDiv);
     for(let j=1; j<=16; j++){
-        const insideDiv = document.createElement('div');
-        insideDiv.classList.add(`insideDiv-${j}`)
-        insideDiv.style.cssText = 'border: 1px solid black; height: 30px; width: 30px';
-        rowDiv.appendChild(insideDiv);
+        const columnDiv = document.createElement('col-div');
+        columnDiv.classList.add(`col-div`)
+        columnDiv.style.cssText = 'border: 1px solid black; height: 30px; width: 30px';
+        rowDiv.appendChild(columnDiv);
+        
+        // window.addEventListener('DOMContentLoaded', function(){
+        //     // hoverForEach(j);
+        // });
+        
     }
 }
+
+const hover = document.querySelectorAll(`.col-div`);
+hover.forEach(element => {
+    element.addEventListener('mouseover', function(){
+        this.style.backgroundColor = `green`;
+    });
+});
