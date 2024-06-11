02221ee2da28d362ae228753a8b8099b92ccf4bb
Animesh Kumar <kumaranimesh923@gmail.com>
2024-06-11 22:21:28 +0530

    fix: add classes and operations elements

--- 05-simple-calculator/script.js
+++ 05-simple-calculator/script.js
@@ -1,10 +1,29 @@
-const innerBox = document.querySelector('.inner-box');
-const rowDiv = document.createElement('row-div');
+const operationsBox = document.querySelector('.operations-box');
+const opsDiv1 = document.createElement('div');
+opsDiv1.classList.add(`addition`);
+opsDiv1.textContent = `+`;
+opsDiv1.style.cssText = `font-size: 50px; border: solid black 2px; padding: 5px;`
+operationsBox.appendChild(opsDiv1);
 
-rowDiv.classList.add(`row-div`);
-rowDiv.textContent = `+`;
+const opsDiv2 = document.createElement('div');
+opsDiv2.classList.add(`subtraction`);
+opsDiv2.textContent = `-`;
+opsDiv2.style.cssText = `font-size: 50px; border: solid black 2px; padding: 5px;`
+operationsBox.appendChild(opsDiv2);
+
+const opsDiv3 = document.createElement('div');
+opsDiv3.classList.add(`mulltiplication`);
+opsDiv3.textContent = `x`;
+opsDiv3.style.cssText = `font-size: 50px; border: solid black 2px; padding: 5px;`
+operationsBox.appendChild(opsDiv3);
+
+const opsDiv4 = document.createElement('div');
+opsDiv4.classList.add(`division`);
+opsDiv4.textContent = `/`;
+opsDiv4.style.cssText = `font-size: 50px; border: solid black 2px; padding: 5px;`
+operationsBox.appendChild(opsDiv4);
 
 
 
-innerBox.appendChild(rowDiv);
 
+// ***************************
