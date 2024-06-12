7cad2aaed692a543e6053a91250fdd3349d781da
Animesh Kumar <kumaranimesh923@gmail.com>
2024-06-13 01:08:36 +0530

    chore: re-arrange css and js, working fine`

--- 05-simple-calculator/script.js
+++ 05-simple-calculator/script.js
@@ -2,6 +2,7 @@ const rowOne = document.querySelector('.row-1');
 const rowTwo = document.querySelector('.row-2');
 const rowThree = document.querySelector('.row-3');
 const rowFour = document.querySelector('.row-4');
+const rowFive = document.querySelector('.row-5');
 
 
 // ----  ROW 1 contents -------------
@@ -9,14 +10,12 @@ for (let i = 7; i <= 9; i++) {
   const numbers = document.createElement('div');
   numbers.classList.add(`num-${i}`);
   numbers.textContent = `${i}`;
-  numbers.style.cssText = `font-size: 50px; border: solid black 2px; padding: 5px; width: 50px; display: flex; justify-content: center;`
   rowOne.appendChild(numbers);
 }
 
 const opsDiv4 = document.createElement('div');
 opsDiv4.classList.add(`division`);
 opsDiv4.textContent = `/`;
-opsDiv4.style.cssText = `font-size: 50px; border: solid black 2px; padding: 5px; width: 50px; display: flex; justify-content: center;`
 rowOne.appendChild(opsDiv4);
 
 // ---------------------------
@@ -27,14 +26,12 @@ for (let i = 4; i <= 6; i++) {
   const numbers = document.createElement('div');
   numbers.classList.add(`num-${i}`);
   numbers.textContent = `${i}`;
-  numbers.style.cssText = `font-size: 50px; border: solid black 2px; padding: 5px; width: 50px; display: flex; justify-content: center;`
   rowTwo.appendChild(numbers);
 }
 
 const opsDiv3 = document.createElement('div');
-opsDiv3.classList.add(`mulltiplication`);
+opsDiv3.classList.add(`multiplication`);
 opsDiv3.textContent = `*`;
-opsDiv3.style.cssText = `font-size: 50px; border: solid black 2px; padding: 5px; width: 50px; display: flex; justify-content: center;`
 rowTwo.appendChild(opsDiv3);
 
 // -------------------------
@@ -46,14 +43,12 @@ for (let i = 1; i <= 3; i++) {
   const numbers = document.createElement('div');
   numbers.classList.add(`num-${i}`);
   numbers.textContent = `${i}`;
-  numbers.style.cssText = `font-size: 50px; border: solid black 2px; padding: 5px; width: 50px; display: flex; justify-content: center;`
   rowThree.appendChild(numbers);
 }
 
 const opsDiv2 = document.createElement('div');
 opsDiv2.classList.add(`subtraction`);
 opsDiv2.textContent = `-`;
-opsDiv2.style.cssText = `font-size: 50px; border: solid black 2px; padding: 5px; width: 50px; display: flex; justify-content: center;`
 rowThree.appendChild(opsDiv2);
 
 // ---------------------------
@@ -64,41 +59,74 @@ rowThree.appendChild(opsDiv2);
 const decimal = document.createElement('div');
 decimal.classList.add(`decimal`);
 decimal.textContent = `.`;
-decimal.style.cssText = `font-size: 50px; border: solid black 2px; padding: 5px; width: 50px; display: flex; justify-content: center;`
 rowFour.appendChild(decimal);
 
 const zero = document.createElement('div');
 zero.classList.add(`zero`);
 zero.textContent = `0`;
-zero.style.cssText = `font-size: 50px; border: solid black 2px; padding: 5px; width: 50px; display: flex; justify-content: center;`
 rowFour.appendChild(zero);
 
 const opsDiv1 = document.createElement('div');
 opsDiv1.classList.add(`addition`);
 opsDiv1.textContent = `+`;
-opsDiv1.style.cssText = `font-size: 50px; border: solid black 2px; padding: 5px; width: 50px; display: flex; justify-content: center;`
 rowFour.appendChild(opsDiv1);
 
 const equal = document.createElement('div');
 equal.classList.add(`equal`);
 equal.textContent = `=`;
-equal.style.cssText = `font-size: 50px; border: solid black 2px; padding: 5px; width: 50px; display: flex; justify-content: center;`
 rowFour.appendChild(equal);
 
 
 // -------------------------------
 
 
+// ----  ROW 5 contents -------------
+
+const clearBtn = document.createElement('button');
+clearBtn.classList.add(`clear-btn`);
+clearBtn.textContent = `clear`;  
+rowFive.appendChild(clearBtn);
 
 
+// -------- OPERATIONS LOGIC --------
+
+let num1, num2, operation;
+
+function add(num1, num2){
+  return num1 + num2;
+}
+
+function subtract(num1, num2){
+  return num1 - num2;
+}
+
+function multiply(num1, num2){
+  return num1 * num2;
+}
+
+function divide(num1, num2){
+  return num1 / num2;
+}
+
+// console.log(add(5, 10), subtract(5, 10), multiply(5, 10), divide(5, 10));
 
 
-
-// NUMBERS   ***************************
-// for(let i=0; i<=9; i++){
-//   const numbers = document.createElement('div');
-//   numbers.classList.add(`num-${i}`);
-//   numbers.textContent = `${i}`;
-//   numbers.style.cssText = `font-size: 50px; border: solid black 2px; padding: 5px; width: 50px; display: flex; justify-content: center;`
-//   operationsBox.appendChild(numbers);
-// }
+function operate(operator, num1, num2){
+  switch (operator) {
+    case '+':
+      add(num1, num2);
+      break;
+    case '-':
+      subtract(num1, num2);
+      break;
+    case '*':
+      multiply(num1, num2);
+      break;
+    case '/':
+      subtract(num1, num2);
+      break;
+    default:
+      alert('not a valid operation')
+      break;
+  }
+}
