ee8113e7fe4640ba1c4e4eca44fd1da82b0cce04
Animesh Kumar <kumaranimesh923@gmail.com>
2024-11-01 12:34:16 +0530

    fix: update script

--- 08-library/script.js
+++ 08-library/script.js
@@ -1,44 +1,70 @@
 const myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, year) {
   // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.year = year;
 }
 
 function addBook() {
-  // do stuff here
+  let book = new Book('Philo', 'Kafka', 278, 2000);
+  myLibrary.push(book);
 }
 
+const displayBtn = document.querySelector('.display-btn');
+
+displayBtn.addEventListener('click', displayBook)
+
+function displayBook(){
+  createCard()
+}
+myLibrary.forEach(item => {
+  
+});
 
 
-const display = document.querySelector('.display');
-const cardDiv = document.createElement('div');
-cardDiv.classList.add('card');
-// cardDiv.textContent = 'first card';
-display.appendChild(cardDiv);
+// for(let i = 0; i < 12; i++){
+// }
 
+// test function for display
+// createCard();
+console.log(myLibrary);
+addBook();
+console.log(myLibrary);
 
-// const card = document.querySelector('.card');
-const titleDiv = document.createElement('div');
-titleDiv.classList.add('title');
-titleDiv.textContent = `Dynamic title`;
-cardDiv.appendChild(titleDiv);
-
-const infoDiv = document.createElement('div');
-infoDiv.classList.add('info');
-// infoDiv.textContent = `hi`
-cardDiv.appendChild(infoDiv);
-
-const authorDiv = document.createElement('div');
-authorDiv.classList.add('meta');
-authorDiv.textContent = `a`;
-infoDiv.appendChild(authorDiv);
-
-const pagesDiv = document.createElement('div');
-pagesDiv.classList.add('meta');
-pagesDiv.textContent = `b`;
-infoDiv.appendChild(pagesDiv);
-
-const yearDiv = document.createElement('div');
-yearDiv.classList.add('meta');
-yearDiv.textContent = `c`
-infoDiv.appendChild(yearDiv);
+function createCard(){
+  // create card
+  const display = document.querySelector('.display');
+  const cardDiv = document.createElement('div');
+  cardDiv.classList.add('card');
+  display.appendChild(cardDiv);
+  
+  // create title
+  const titleDiv = document.createElement('div');
+  titleDiv.classList.add('title');
+  titleDiv.textContent = `Dynamic title`;
+  cardDiv.appendChild(titleDiv);
+  
+  // create info
+  const infoDiv = document.createElement('div');
+  infoDiv.classList.add('info');
+  infoDiv.textContent = `info`
+  cardDiv.appendChild(infoDiv);
+  
+  const authorDiv = document.createElement('div');
+  authorDiv.classList.add('meta');
+  authorDiv.textContent = `a`;
+  infoDiv.appendChild(authorDiv);
+  
+  const pagesDiv = document.createElement('div');
+  pagesDiv.classList.add('meta');
+  pagesDiv.textContent = `b`;
+  infoDiv.appendChild(pagesDiv);
+  
+  const yearDiv = document.createElement('div');
+  yearDiv.classList.add('meta');
+  yearDiv.textContent = `c`
+  infoDiv.appendChild(yearDiv);
+}
