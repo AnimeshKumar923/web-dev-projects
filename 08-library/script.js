08755f4dec1acf128742e7bedec1da1b7c27baec
Animesh Kumar <kumaranimesh923@gmail.com>
2024-11-01 10:55:10 +0530

    fix: update structure, style and script

--- 08-library/script.js
+++ 08-library/script.js
@@ -1,7 +1,20 @@
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBook() {
+  // do stuff here
+}
+
+
+
 const display = document.querySelector('.display');
 const card = document.createElement('div');
 card.classList.add('card');
 card.textContent = 'first card';
-// card.style.cssText = 'border: 5px solid rgb(32, 155, 226); display: grid; grid-template-columns: repeat(2, 1fr)';
+
+
 display.appendChild(card);
 // display.appendChild('.card');
