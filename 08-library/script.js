f166950a4e32c5c0a33edf7d86c8531973b10529
Animesh Kumar <kumaranimesh923@gmail.com>
2024-11-01 11:18:23 +0530

    fix: dom manipulation

--- 08-library/script.js
+++ 08-library/script.js
@@ -11,10 +11,34 @@ function addBook() {
 
 
 const display = document.querySelector('.display');
-const card = document.createElement('div');
-card.classList.add('card');
-card.textContent = 'first card';
+const cardDiv = document.createElement('div');
+cardDiv.classList.add('card');
+// cardDiv.textContent = 'first card';
+display.appendChild(cardDiv);
 
 
-display.appendChild(card);
-// display.appendChild('.card');
+// const card = document.querySelector('.card');
+const titleDiv = document.createElement('div');
+titleDiv.classList.add('title');
+titleDiv.textContent = `Dynamic title`;
+cardDiv.appendChild(titleDiv);
+
+const infoDiv = document.createElement('div');
+infoDiv.classList.add('info');
+// infoDiv.textContent = `hi`
+cardDiv.appendChild(infoDiv);
+
+const authorDiv = document.createElement('div');
+authorDiv.classList.add('meta');
+authorDiv.textContent = `a`;
+infoDiv.appendChild(authorDiv);
+
+const pagesDiv = document.createElement('div');
+pagesDiv.classList.add('meta');
+pagesDiv.textContent = `b`;
+infoDiv.appendChild(pagesDiv);
+
+const yearDiv = document.createElement('div');
+yearDiv.classList.add('meta');
+yearDiv.textContent = `c`
+infoDiv.appendChild(yearDiv);
