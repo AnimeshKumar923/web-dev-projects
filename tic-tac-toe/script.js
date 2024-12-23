90d9aadbdc3ee8ca509c7a9a4182d73622faf1e5
Animesh Kumar <kumaranimesh923@gmail.com>
2024-12-23 11:02:10 +0530

    update game logic

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,17 +1,35 @@
 const gameBoard = (function(){
-  let grid = [null, null, null, null, null, null, null, null, null];
+  // let grid = [null, null, null, null, null, null, null, null, null];
+  let grid = [];
+  for (let i = 0; i < 9; i++) {
+    grid.push({move: null, isOccupied: false});
+  }
+  let gridPosition = Number(prompt('take your move from 1-9'));
+  // playerMove();
+  const playerOne = new Player('ONE', 'x');
+  const playerTwo = new Player('TWO', 'o');
+  
+  function playerTurn(){
+    if(playerOne.isOccupied === true){
+      prompt('enter different position');
+    }
+
+    if(playerOne.role === 'x' && playerOne.isOccupied === false){
+      grid[gridPosition].move = 'x';
+      grid[gridPosition].isOccupied = true;
+    } else {
+      if(playerTwo.isOccupied === false){
+        grid[gridPosition].move = 'o';
+        grid[gridPosition].isOccupied = true;
+      }
+      
+    }
+    
+  }
+
+  const Player = function(name, role){
+    this.name = name;
+    this.playerId = playerId;
+    this.role = role // choose role X or O
+  }
 })();
-
-const playerMove = function(){
-  let movePosition = Number(prompt('take your move from 1-9'));
-  console.log(movePosition);
-  // console.log(typeof movePosition);
-
-}
-
-const Player = function(name){
-  this.name = name;
-  this.playerId = playerId;
-}
-
-// playerMove();
